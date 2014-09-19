46c9aa0476acebcd599d40bb2fe83a45ffc46538
gilmario-kpslow <gilmariooftware.com>
2014-09-18 21:41:25 -0300

    Criando cadastros basicos

--- atus/src/java/br/com/atus/controller/PecaController.java
+++ atus/src/java/br/com/atus/controller/PecaController.java
@@ -6,11 +6,26 @@
 package br.com.atus.controller;
 
 import br.com.atus.dao.PecaDAO;
+import br.com.atus.exceptions.PecaFileException;
 import br.com.atus.modelo.Peca;
 import br.com.atus.util.ArquivoUtil;
+import br.com.atus.util.DocumentoConverter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import org.primefaces.model.UploadedFile;
 
 /**
@@ -24,6 +39,7 @@ public class PecaController extends Controller<Peca, Long> implements Serializab
     private PecaDAO dao;
 
     @Override
+    @PostConstruct
     protected void inicializaDAO() {
         setDAO(dao);
     }
@@ -31,8 +47,30 @@ public class PecaController extends Controller<Peca, Long> implements Serializab
     public void salvar(Peca t, UploadedFile file) throws Exception {
         t.setArquivo(file.getFileName());
         super.salvar(t);
-        // Salvar o aquivo aqui
-        ArquivoUtil.gravaArquivo("pecas/documentos", file.getFileName(), file.getContents());
+        ArquivoUtil.gravaArquivo(t.getId().toString(), file.getFileName(), file.getContents());
+    }
+
+    public StreamedContent getDownload(Peca p) throws PecaFileException, FileNotFoundException {
+        List<File> files = ArquivoUtil.aquivos(p.getId().toString());
+        if (files.isEmpty()) {
+            throw new PecaFileException();
+        }
+        File f = files.get(0);
+        return new DefaultStreamedContent(new FileInputStream(f), "docx", p.getArquivo());
+    }
+
+    public StreamedContent getModeloDownload(Peca peca, Object entidade) throws PecaFileException, FileNotFoundException, Docx4JException, IllegalArgumentException, IllegalAccessException {
+        InputStream stream = getDownload(peca).getStream();
+        return DocumentoConverter.converterArquivo(stream, entidade, peca);
+    }
+
+    public boolean validaArquivoDocx(UploadedFile f, String grupo) {
+        try {
+            return DocumentoConverter.validarArquivo(f.getInputstream(), Class.forName(grupo));
+        } catch (IOException | ClassNotFoundException | Docx4JException ex) {
+            Logger.getLogger(PecaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
 }
