19ac036f7aa9ec3022bea91ac81d2a65e30d328b
aricomputacao <aricomputacao@gmail.com>
2014-10-23 10:46:09 -0300

    movimentaçoes

---
+++ AtusModelo/src/br/com/atus/modelo/SubGrupoPeca.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.atus.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author Ari
+ */
+@Entity
+@Table(name = "sub_grupo_peca",schema = "processo")
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class SubGrupoPeca implements Serializable{
+     @Id
+    @Column(name = "sub_grp_id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @NotBlank
+    @Column(name = "sub_grp_nome",nullable = false)
+    private String nome;
+    
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "grp_id",referencedColumnName = "grp_id",nullable = false)
+    private GrupoPeca grupoPeca;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public GrupoPeca getGrupoPeca() {
+        return grupoPeca;
+    }
+
+    public void setGrupoPeca(GrupoPeca grupoPeca) {
+        this.grupoPeca = grupoPeca;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SubGrupoPeca other = (SubGrupoPeca) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
