8dafc3e88cde48c53a6358362a1dd255e21776cb
aricomputacao <aricomputacao@gmail.com>
2014-10-23 11:17:20 -0300

    Merge origin/master
    Conflicts:
    	atus/src/conf/persistence.xml

--- atus/src/java/br/com/atus/controller/AdversarioController.java
+++ atus/src/java/br/com/atus/controller/AdversarioController.java
@@ -1,13 +1,13 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * and open the template in the editor. xx
+ */ 
 
 package br.com.atus.controller;
 
 import br.com.atus.dao.AdversarioDAO;
-import br.com.atus.modelo.Adversario;
+import br.com.atus.modelo.Adversario; 
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
